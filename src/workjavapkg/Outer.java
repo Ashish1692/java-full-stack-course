ad49dcbb44905e9605879a974bbcfa5ae73bd17b
Ashish1692 <ashishmoghe6@gmail.com>
2023-07-19 16:39:15 +0530

    outer class code started

---
+++ src/workjavapkg/Outer.java
@@ -0,0 +1,15 @@
+package workjavapkg;
+
+public class Outer {
+	int a = 19;
+
+	class Inner{
+		void display() {
+			System.out.println(a);
+		}
+	}
+	public static void main(String[] args) {
+
+	}
+
+}

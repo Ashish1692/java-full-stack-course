c0827fcae8ba116654fa018e7e2b31cb030b8c2b
Ashish1692 <ashishmoghe6@gmail.com>
2023-07-25 17:28:46 +0530

    createing thread

---
+++ src/workjavapkg/CreateThread.java
@@ -0,0 +1,27 @@
+package workjavapkg;
+
+public class CreateThread extends Thread{
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName());
+		for(int i = 0; i<10;i++) {
+			System.out.println("["+Thread.currentThread().getName()+"]= "+i);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("main() called");
+		System.out.println(Thread.currentThread().getName());
+		// process 1
+		CreateThread t= new CreateThread();
+		t.start();
+		// process 2
+		CreateThread t1= new CreateThread();
+		t1.start();
+		// process 3
+		CreateThread t2 = new CreateThread();
+		t2.start();
+		
+	}
+
+}
--- src/workjavapkg/ThreadBasic.java
+++ src/workjavapkg/ThreadBasic.java
@@ -1,13 +1,17 @@
 package workjavapkg;
 
 public class ThreadBasic {
+	static {
+		System.out.println("Static{} -> Current Thread: "+Thread.currentThread().getName());
+		System.out.println("Static{} -> Current Thread's Priority: "+Thread.currentThread().getPriority());
+	}
 
 	public static void main(String[] args) {
 		/**
 		 * default thread is main and it is automatically created by jvm 
 		 *
 		 */
-		System.out.println("Current Thread: "+Thread.currentThread().getName());
+		System.out.println("\nCurrent Thread: "+Thread.currentThread().getName());
 		System.out.println("Current Thread's Priority: "+Thread.currentThread().getPriority());
 
 	}

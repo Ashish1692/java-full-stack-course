bcc6e9657541a884981e8557d37ecd740b1df84f
Ashish1692 <ashishmoghe6@gmail.com>
2023-08-10 12:10:12 +0530

    insert query created

--- src/database/InsertSql.java
+++ src/database/InsertSql.java
@@ -1,7 +1,7 @@
 package database;
 
 import java.sql.*;
-
+import java.util.Scanner;
 public class InsertSql {
 	public static void main(String[] args) throws SQLException{
 		// JDBC URL, user_name, and password
@@ -19,21 +19,33 @@ public class InsertSql {
 			// Create a statement object
 			Statement statement = connection.createStatement();
 
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter id: ");
+			int sysid = scanner.nextInt();
+			scanner.nextLine();
+			System.out.println("Enter name: ");
+			String sysname = scanner.nextLine();
+			
+			
 			// Execute the SELECT query
-			String sqlQuery = "Insert into testtable (id,name,code) values(?,?,?)";
-			ResultSet resultSet = statement.executeQuery(sqlQuery);
+//			String sqlQuery = "Insert into test values(3,'alex')";
+			String sqlQuery = "Insert into test(id,name)" + "values("+sysid+",'"+sysname+"')";
+			int affected = statement.executeUpdate(sqlQuery);
+			System.out.println("row added: "+affected);
+//			ResultSet resultSet = statement.executeQuery(sqlQuery);
 
 			// Process the result set
-			while (resultSet.next()) {
-				// Assuming your_table_name has columns 'column1' and 'column2'
-				String column1Value = resultSet.getString("id");
-				String column2Value = resultSet.getString("name");
-				System.out.println("id: " + column1Value + ", name: " + column2Value);
-			}
+//			while (resultSet.next()) {
+//				// Assuming your_table_name has columns 'column1' and 'column2'
+//				String column1Value = resultSet.getString("id");
+//				String column2Value = resultSet.getString("name");
+//				System.out.println("id: " + column1Value + ", name: " + column2Value);
+//			}
 
 			// Close the resources
-			resultSet.close();
+//			resultSet.close();
 			statement.close();
+			scanner.close();
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("MySQL JDBC driver not found!");
